573b140ed43f87a5a1a12f7f054987b81da5e41c
Spencer B <Cendeu@gmail.com>
2021-12-25 23:24:45 -0600

    feat: :tada: start using git
    Started using Git here. Should have earlier, but better late than never

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopCardComponent } from './top-card/top-card.component';
+import { AboutComponent } from './about/about.component';
+import { StackComponent } from './stack/stack.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { CreditsComponent } from './credits/credits.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopCardComponent,
+    AboutComponent,
+    StackComponent,
+    FooterComponent,
+    ContactComponent,
+    CreditsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
